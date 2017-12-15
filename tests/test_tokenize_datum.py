1bfb36c732efccaa3acfc7813d71c34d52f93fc7
Solumilken <s916526000@gmail.com>
2017-12-15 12:01:41 +0800

    tokenize datum

---
+++ tests/test_tokenize_datum.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+from unittest import TestCase
+from unittest.mock import Mock
+from uttut.elements import (
+    Datum,
+    Intent,
+    Entity,
+)
+from ..tokenize_datum import (
+    tokenize_datum,
+    _get_entity_array,
+)
+
+
+class TokenizeDatumTestCase(TestCase):
+
+    def setUp(self):
+        self.example_datum = Datum(
+            utterance='家豪大大喜歡吃豪大雞排',
+            intents=[Intent(name='favor')],
+            entities=[
+                Entity(name='brand', value='豪大', start=7, end=9,
+                       replacements=['肯德基', '麥噹噹']),
+                Entity(name='food', value='雞排', start=9, end=11,
+                       replacements=['漢堡', '薯條', '滷肉飯']),
+            ],
+        )
+        self.fake_tokenizer = Mock()
+
+    def test__get_entity_array(self):
+        result = _get_entity_array(
+            datum=self.example_datum,
+        )
+        self.assertEqual(
+            ['DONT_CARE', 'DONT_CARE', 'DONT_CARE', 'DONT_CARE', 'DONT_CARE',
+             'DONT_CARE', 'DONT_CARE', 'brand', 'brand', 'food', 'food'],
+            result,
+        )
+
+    def test_tokenize_datum(self):
+        self.fake_tokenizer.lcut = Mock(
+            return_value=['家豪大大喜歡吃豪', '大雞排'],
+        )
+        result = tokenize_datum(
+            datum=self.example_datum,
+            tokenizer=self.fake_tokenizer,
+        )
+        self.assertEqual(
+            ['brand', 'food'],
+            result[1],
+        )
+        self.fake_tokenizer.lcut = Mock(
+            return_value=['家豪大大', '喜歡吃豪', '大雞', '排'],
+        )
+        result = tokenize_datum(
+            datum=self.example_datum,
+            tokenizer=self.fake_tokenizer,
+        )
+        self.assertEqual(
+            ['DONT_CARE', 'brand', 'brand', 'food'],
+            result[1],
+        )
---
+++ tokenize_datum.py
@@ -0,0 +1,42 @@
+from typing import List
+
+from uttut.elements import Datum
+from uttut import ENTITY_LABEL
+
+from collections import Counter
+
+
+def _get_entity_array(
+        datum: Datum,
+        not_entity: str = ENTITY_LABEL['NOT_ENTITY'],
+    ):
+    result = [not_entity] * len(datum.utterance)
+    for entity in datum.entities:
+        for ind in range(entity.start, entity.end):
+            result[ind] = entity.name
+    return result
+
+
+def tokenize_datum(
+        datum: Datum,
+        tokenizer: object,
+        not_entity: str = ENTITY_LABEL['NOT_ENTITY'],
+    ) -> (List[str], List[str]):
+
+    tokenized_utterance = tokenizer.lcut(datum.utterance)
+    entity_array = _get_entity_array(
+        datum=datum,
+        not_entity=not_entity,
+    )
+    entities = []
+    begin_ind = 0
+    for token in tokenized_utterance:
+        entity_stat = Counter(
+            entity_array[begin_ind: begin_ind + len(token)],
+        ).most_common()
+        if (entity_stat[0][0] == not_entity) and (len(entity_stat) > 1):
+            entities.append(entity_stat[1][0])
+        else:
+            entities.append(entity_stat[0][0])
+        begin_ind += len(token)
+    return tokenized_utterance, entities
