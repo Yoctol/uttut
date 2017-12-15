706eec15bcda99978726e9b1b1181adc2c9efb73
Solumilken <s916526000@gmail.com>
2017-12-15 12:01:17 +0800

    normalize_datum

---
+++ normalize_datum.py
@@ -0,0 +1,105 @@
+from typing import List
+
+from uttut.elements import Datum, Entity
+from uttut import ENTITY_LABEL
+
+
+def _gen_partitioned_utterance_n_entities(
+        datum: Datum,
+        not_entity: str = ENTITY_LABEL['NOT_ENTITY'],
+    ) -> (List[List[str]], List[List[str]]):
+
+    start = 0
+    utterance = datum.utterance
+    partitioned_utterance = []
+    partitioned_entities = []
+
+    if datum.has_entities():
+        for entity in datum.entities:
+            if start != entity.start:
+                partitioned_utterance += [
+                    utterance[start: entity.start]]
+                partitioned_entities.append(not_entity)
+            partitioned_utterance += [
+                utterance[entity.start: entity.end]]
+            partitioned_entities.append(entity.name)
+            start = entity.end
+
+        if datum.entities[-1].end != len(utterance):
+            partitioned_utterance += [utterance[start:]]
+            partitioned_entities.append(not_entity)
+    else:
+        partitioned_utterance = [utterance]
+        partitioned_entities = [not_entity]
+
+    if len(partitioned_utterance) != len(partitioned_entities):
+        raise KeyError(
+            'Number of segments of utterance and entities is not equal',
+            'partitioned_utterance = {}, \n partitioned_entities = {}'.format(
+                partitioned_utterance, partitioned_entities,
+            ),
+        )
+
+    return partitioned_utterance, partitioned_entities
+
+
+def normalize_datum(
+        datum: Datum,
+        text_normalizer: object = None,
+        not_entity=ENTITY_LABEL['NOT_ENTITY'],
+    ) -> (Datum, List[dict]):
+
+    if text_normalizer is None:
+        return datum, None
+
+    normalized_utterance, meta = text_normalizer.normalize(
+        sentence=datum.utterance,
+    )
+    if not datum.entities:
+        return Datum(
+            utterance=normalized_utterance,
+            intents=datum.intents,
+        ), None
+    else:
+        partitioned_utterance, partitioned_entities = \
+            _gen_partitioned_utterance_n_entities(
+                datum=datum,
+                not_entity=not_entity,
+            )
+        begin_ind = 0
+        entities = []
+        entities_ind = 0
+        for segment, entity in zip(partitioned_utterance, partitioned_entities):
+            normalized_segment, _ = text_normalizer.normalize(
+                sentence=segment,
+            )
+            start = normalized_utterance.find(
+                normalized_segment,
+                begin_ind,
+            )
+            if start == -1:
+                raise KeyError(
+                    'String match fails, str = {}, substr = {}'.format(
+                        normalized_utterance,
+                        normalized_segment,
+                    ),
+                )
+
+            begin_ind = start + len(normalized_segment)
+            if entity != not_entity:
+                entities.append(
+                    Entity(
+                        name=entity,
+                        value=normalized_segment,
+                        start=start,
+                        end=begin_ind,
+                        replacements=datum.entities[entities_ind].replacements,
+                    ),
+                )
+                entities_ind += 1
+
+    return Datum(
+        utterance=normalized_utterance,
+        intents=datum.intents,
+        entities=entities,
+    ), meta
---
+++ tests/test_normalize_datum.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+# import re
+from unittest import TestCase
+from unittest.mock import call, Mock
+from uttut.elements import (
+    Datum,
+    Intent,
+    Entity,
+)
+from ..normalize_datum import (
+    _gen_partitioned_utterance_n_entities,
+    normalize_datum,
+)
+
+
+class FakeTextNormalizer(object):
+
+    def normalize(self, sentence):
+        return sentence, None
+
+
+class NormalizeDatumTestCase(TestCase):
+
+    def setUp(self):
+        self.example_datum = Datum(
+            utterance='家豪大大喜歡吃豪大雞排',
+            intents=[Intent(name='favor')],
+            entities=[
+                Entity(name='brand', value='豪大', start=7, end=9,
+                       replacements=['肯德基', '麥噹噹']),
+                Entity(name='food', value='雞排', start=9, end=11,
+                       replacements=['漢堡', '薯條', '滷肉飯']),
+            ],
+        )
+
+    def test__gen_partitioned_utterance_n_entities(self):
+        result = _gen_partitioned_utterance_n_entities(
+            datum=self.example_datum,
+        )
+        self.assertEqual(
+            (['家豪大大喜歡吃', '豪大', '雞排'], ['DONT_CARE', 'brand', 'food']),
+            result,
+        )
+        result = _gen_partitioned_utterance_n_entities(
+            datum=self.example_datum,
+            not_entity='HAHAHA',
+        )
+        self.assertEqual(
+            (['家豪大大喜歡吃', '豪大', '雞排'], ['HAHAHA', 'brand', 'food']),
+            result,
+        )
+
+    def test_normalize_datum(self):
+        result = normalize_datum(
+            datum=self.example_datum,
+            text_normalizer=None,
+        )
+        self.assertEqual(
+            None,
+            result[1],
+        )
+        self.assertEqual(
+            self.example_datum,
+            result[0],
+        )
+        self.fake_text_normalizer = FakeTextNormalizer()
+        result = normalize_datum(
+            datum=self.example_datum,
+            text_normalizer=self.fake_text_normalizer,
+        )
+        self.assertEqual(
+            None,
+            result[1],
+        )
+        self.assertEqual(
+            self.example_datum,
+            result[0],
+        )
+        self.fake_text_normalizer.normalize = Mock(return_value=('', None))
+        normalize_datum(
+            datum=self.example_datum,
+            text_normalizer=self.fake_text_normalizer,
+        )
+        self.fake_text_normalizer.normalize.assert_has_calls(
+            [
+                call(sentence='家豪大大喜歡吃豪大雞排'),
+                call(sentence='家豪大大喜歡吃'),
+                call(sentence='豪大'),
+                call(sentence='雞排'),
+            ],
+        )
