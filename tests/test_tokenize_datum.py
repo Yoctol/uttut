9704205741192c2070102b627ae1c932d815bb39
Solumilken <s916526000@gmail.com>
2018-01-03 18:45:23 +0800

    added test case

--- tests/test_tokenize_datum.py
+++ tests/test_tokenize_datum.py
@@ -15,7 +15,7 @@ from ..tokenize_datum import (
 class TokenizeDatumTestCase(TestCase):
 
     def setUp(self):
-        self.example_datum = Datum(
+        self.example_datum_1 = Datum(
             utterance='家豪大大喜歡吃豪大雞排',
             intents=[Intent(name='favor')],
             entities=[
@@ -25,11 +25,21 @@ class TokenizeDatumTestCase(TestCase):
                        replacements=['漢堡', '薯條', '滷肉飯']),
             ],
         )
+        self.example_datum_2 = Datum(
+            utterance='家豪大大 喜歡 吃 豪大 雞排',
+            intents=[Intent(name='favor')],
+            entities=[
+                Entity(name='brand', value='豪大', start=10, end=12,
+                       replacements=['肯德基', '麥噹噹']),
+                Entity(name='food', value='雞排', start=13, end=15,
+                       replacements=['漢堡', '薯條', '滷肉飯']),
+            ],
+        )
         self.fake_tokenizer = Mock()
 
     def test__get_entity_array(self):
         result = _get_entity_array(
-            datum=self.example_datum,
+            datum=self.example_datum_1,
         )
         self.assertEqual(
             ['DONT_CARE', 'DONT_CARE', 'DONT_CARE', 'DONT_CARE', 'DONT_CARE',
@@ -37,12 +47,12 @@ class TokenizeDatumTestCase(TestCase):
             result,
         )
 
-    def test_tokenize_datum(self):
+    def test_tokenize_datum_1(self):
         self.fake_tokenizer.lcut = Mock(
             return_value=['家豪大大喜歡吃豪', '大雞排'],
         )
         result = tokenize_datum(
-            datum=self.example_datum,
+            datum=self.example_datum_1,
             tokenizer=self.fake_tokenizer,
         )
         self.assertEqual(
@@ -53,10 +63,23 @@ class TokenizeDatumTestCase(TestCase):
             return_value=['家豪大大', '喜歡吃豪', '大雞', '排'],
         )
         result = tokenize_datum(
-            datum=self.example_datum,
+            datum=self.example_datum_1,
             tokenizer=self.fake_tokenizer,
         )
         self.assertEqual(
             ['DONT_CARE', 'brand', 'brand', 'food'],
+            result[1],
+        )
+
+    def test_tokenize_datum_2(self):
+        self.fake_tokenizer.lcut = Mock(
+            return_value=['家豪大大', '喜歡', '吃', '豪大', '雞排'],
+        )
+        result = tokenize_datum(
+            datum=self.example_datum_2,
+            tokenizer=self.fake_tokenizer,
+        )
+        self.assertEqual(
+            ['DONT_CARE', 'DONT_CARE', 'DONT_CARE', 'brand', 'food'],
             result[1],
         )
