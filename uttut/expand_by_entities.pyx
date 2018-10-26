a63d76cf3126aafaed2f2cf39a3e6087189b19c7
cph <stegben.benjamin@gmail.com>
2018-10-26 19:06:50 +0800

    rewrite expand_by_entities

--- uttut/expand_by_entities.pyx
+++ uttut/expand_by_entities.pyx
@@ -1,12 +1,14 @@
 # cython: profile=True
 # cython: linetrace=False
 import random
-from typing import List, Tuple
+from typing import List, Tuple, Union
 import numpy as np
+
 from .elements import (
     Datum,
     Entity,
 )
+from .toolkits.get_kth_combination import get_kth_combination
 
 
 __all__ = [
@@ -14,241 +16,85 @@ __all__ = [
 ]
 
 
-cdef int greatest_common_divisor(int x, int y) nogil:
-    """This function implements the Euclidian algorithm
-    to find G.C.D. of two numbers"""
-    while y:
-        x, y = y, x % y
-    return x
-
-
-cdef int least_common_multiple(int x, int y) nogil:
-    """This function takes two
-    integers and returns the L.C.M."""
-
-    cdef int lcm = (x * y) // greatest_common_divisor(x, y)
-    return lcm
-
-
-cdef int least_common_multiple_for_list_of_number(list list_of_number):
-    cdef int lcm
-    cdef int number
-    cdef int idx
-    cdef int len_list_of_number = len(list_of_number)
-    for idx in range(len_list_of_number):
-        number = list_of_number[idx]
-        if idx == 0:
-            lcm = number
-        else:
-            lcm = least_common_multiple(lcm, number)
-    return lcm
-
-
-def augment_single_partition_utterances(
-        partitioned_utterance,
-        sorted_entities,  # List[Entity]
-        random_shuffle=False,
-        random_seed=2018,
-    ):
-    cdef list n_replacements = [entity.n_replacements() for entity in sorted_entities]
-    cdef int lcm = least_common_multiple_for_list_of_number(n_replacements)
-
-    complete_set_size = max(n_replacements)
-
-    if complete_set_size > 1:
-        sorted_entities = sorted(
-            sorted_entities,
-            key=lambda x: x.n_replacements(),
-        )
-
-        #### TODO: NEED BETTER THRESHOLD
-        if lcm > 5000:
-            data_size = int(5000 + np.sqrt(lcm - 5000))
-            if data_size < complete_set_size:
-                data_size = complete_set_size
-        else:
-            data_size = lcm
-
-        output = _augment_partition_utterance_number_less_than_lcm(
-            partitioned_utterance=partitioned_utterance,
-            sorted_entities=sorted_entities,
-            data_size=data_size,
-            random_shuffle=random_shuffle,
-            random_seed=random_seed,
-        )
-    else:
-        output = [partitioned_utterance]
-    return output
-
-
-def augment_single_partition_utterances_with_upper_bound(
-        partitioned_utterance,
-        sorted_entities,  # List[Entity]
-        upper_bound,
-        random_shuffle=False,
-        random_seed=2018,
-    ):
-    cdef list n_replacements = [entity.n_replacements() for entity in sorted_entities]
-    cdef lcm = least_common_multiple_for_list_of_number(n_replacements)
-
-    if lcm > upper_bound:
-        data_size = upper_bound
-    else:
-        data_size = lcm
-    complete_set_size = max(n_replacements)
-
-    output = _augment_partition_utterance_number_less_than_lcm(
-        partitioned_utterance=partitioned_utterance,
-        sorted_entities=sorted_entities,
-        data_size=data_size,
-        random_shuffle=random_shuffle,
-        random_seed=random_seed,
-    )
-    return output
-
-
-cdef list _augment_partition_utterance_number_less_than_lcm(
-        list partitioned_utterance,  # List[str]
-        list sorted_entities,  # List[Entity]
-        int data_size,
-        bint random_shuffle=False,
-        int random_seed=2018,
-    ):
-    cdef list init_indices = []
-    cdef list output = []
-
-    random.seed(random_seed)
-    if random_shuffle:
-        init_indices = [random.randrange(entity.n_replacements()) for entity in sorted_entities]
-    else:
-        init_indices = [0 for _ in range(len(sorted_entities))]
-
-    for idx in range(data_size):
-        partitioned_utterance_copy = partitioned_utterance[:]
-        for entity, init_index in zip(sorted_entities, init_indices):
-            if entity.no_replacements():
-                continue
-            r_idx = (idx + init_index) % entity.n_replacements()
-            if r_idx != 0:
-                partitioned_utterance_copy[entity.index] = \
-                    list(entity.replacements)[r_idx - 1]
-        output += [partitioned_utterance_copy]
-    return output
-
-
-def gen_partitioned_utterance(
-        utterance: str,
-        sorted_entities: list,  # List[Entity]
-    ) -> Tuple[List, List]:
-
+def partition_by_entities(
+        datum: Datum,
+        include_orig: bool,
+    ) -> Tuple[List[List[str]], List[Union[None, str]]]:
     start = 0
-    partitioned_utterance = []
+    parts = []
+    entity_names = []
+    utt = datum.utterance
 
-    for num, entity in enumerate(sorted_entities):
+    for entity in datum.entities:
         if start != entity.start:
-            partitioned_utterance += [utterance[start: entity.start]]
-        partitioned_utterance += [utterance[entity.start: entity.end]]
-        sorted_entities[num].index = len(partitioned_utterance) - 1
+            parts.append([utt[start: entity.start]])
+            entity_names.append(None)
+
+        if entity.no_replacements():
+            part_candidates = [entity.value]
+        else:
+            part_candidates = sorted(list(entity.replacements))
+            if include_orig:
+                part_candidates.append(entity.value)
+        parts.append(part_candidates)
+        entity_names.append(entity.name)
+
         start = entity.end
 
-    if sorted_entities[-1].end != len(utterance):
-        partitioned_utterance += [utterance[start:]]
+    if start != len(utt):
+        parts.append([utt[start:]])
+        entity_names.append(None)
 
-    return sorted_entities, partitioned_utterance
-
-
-def partition_by_entities(datum: Datum) -> Tuple[List[dict], List[dict]]:
-    # entities = [entity.to_dict() for entity in datum.entities]
-    new_entities, parts = gen_partitioned_utterance(
-        sorted_entities=datum.entities,
-        utterance=datum.utterance,
-    )
-    return new_entities, parts
+    return parts, entity_names
 
 
-cdef _include_orig_value(
-        entities_with_index: List[Entity],
-        parts: List[str],
+def _aggregate_entities(
+        list segments,
+        list entity_names,
     ):
-    new_entities = entities_with_index.copy()
-    for entity in new_entities:
-        target_idx = entity['index']
-        replacements = entity.get('replacements', [])
-        replacements.append(parts[target_idx])
-        entity['replacements'] = replacements
-    return new_entities
-
-
-cdef _aggregate_entities(
-        list parts,
-        list entities_with_index,
-        bint include_replacements,
-    ):
-
     cdef list entities = []
     cdef int pointer = 0
-    cdef int idx
-    cdef int n_parts = len(parts)
-    cdef int n_entities = len(entities_with_index)
-    cdef int len_part
-    cdef set replacements
-    cdef str part
-    cdef dict index2entity = {}
+    cdef int len_segment
 
-    for idx in range(n_entities):
-        obj = entities_with_index[idx]
-        index2entity[obj.index] = obj
+    for seg, entity_name in zip(segments, entity_names):
+        len_seg = len(seg)
+        if entity_name is None:
+            pointer += len_seg
+            continue
 
-    for idx in range(n_parts):
-        part = parts[idx]
-        len_part = len(part)
-        if idx in index2entity:
-            entity = index2entity[idx]
-            if include_replacements:
-                replacements = entity.replacements_with_self - set([part])
-                entities.append(Entity(
-                    name=entity.name,
-                    value=part,
-                    start=pointer,
-                    end=pointer + len_part,
-                    replacements=list(replacements),
-                ))
-            else:
-                entities.append(Entity(
-                    name=entity.name,
-                    value=part,
-                    start=pointer,
-                    end=pointer + len_part,
-                ))
-        pointer += len_part
+        entity = Entity(
+            name=entity_name,
+            value=seg,
+            start=pointer,
+            end=pointer + len_seg
+        )
+        entities.append(entity)
+        pointer += len_seg
     return entities
 
 
-cpdef expand_by_entities(
+def expand_by_entities(
         datum,
-        augment_method,
-        dict augment_params=None,
-        bint include_replacements=False,
+        sampling_method = None,
+        bint include_orig = False,
     ):
-    if augment_params is None:
-        augment_params = {}
-
     if not datum.has_entities():
         return [datum]
-    entities_with_index, parts = partition_by_entities(datum)
 
-    augmented_parts = augment_method(
-        partitioned_utterance=parts,
-        sorted_entities=entities_with_index,
-        **augment_params,
-    )
+    if sampling_method is None:
+        sampling_method = lambda k: list(range(k))  # return all possible combinations
 
-    # included_entities_with_index = _include_orig_value(entities_with_index, parts)
+    parts, entity_names = partition_by_entities(datum, include_orig)
+
+    n_combinations = np.prod([len(part) for part in parts])
+    ints = sampling_method(n_combinations)
 
     result = []
-    for parts in augmented_parts:
-        utterance = ''.join(parts)
-        entities = _aggregate_entities(parts, entities_with_index, include_replacements)
+    for idx in ints:
+        segments = get_kth_combination(parts, idx)
+
+        utterance = ''.join(segments)
+        entities = _aggregate_entities(segments, entity_names)
         new_datum = Datum(
             utterance=utterance,
             intents=datum.copy_intents(),
--- uttut/tests/test_expand_by_entities.py
+++ uttut/tests/test_expand_by_entities.py
@@ -3,8 +3,7 @@ from unittest import TestCase
 from ..expand_by_entities import (
     expand_by_entities,
     partition_by_entities,
-    augment_single_partition_utterances,
-    augment_single_partition_utterances_with_upper_bound,
+    _aggregate_entities,
 )
 from ..elements import (
     Datum,
@@ -40,38 +39,6 @@ class ExpandByEntitiesTestCase(TestCase):
             '我想訂下禮拜二從紐約飛到KIX的機票',
         ]
         self.expected_entity_lists = [
-            [
-                Entity(name='日期', value='明天', start=3, end=5, replacements=['下禮拜二']),
-                Entity(name='出發地', value='紐約', start=6, end=8),
-                Entity(name='目的地', value='新加坡', start=10, end=13, replacements=['斯堪地那維亞', 'KIX']),
-            ],
-            [
-                Entity(name='日期', value='下禮拜二', start=3, end=7, replacements=['明天']),
-                Entity(name='出發地', value='紐約', start=8, end=10),
-                Entity(name='目的地', value='新加坡', start=12, end=15, replacements=['斯堪地那維亞', 'KIX']),
-            ],
-            [
-                Entity(name='日期', value='明天', start=3, end=5, replacements=['下禮拜二']),
-                Entity(name='出發地', value='紐約', start=6, end=8),
-                Entity(name='目的地', value='斯堪地那維亞', start=10, end=16, replacements=['新加坡', 'KIX']),
-            ],
-            [
-                Entity(name='日期', value='下禮拜二', start=3, end=7, replacements=['明天']),
-                Entity(name='出發地', value='紐約', start=8, end=10),
-                Entity(name='目的地', value='斯堪地那維亞', start=12, end=18, replacements=['新加坡', 'KIX']),
-            ],
-            [
-                Entity(name='日期', value='明天', start=3, end=5, replacements=['下禮拜二']),
-                Entity(name='出發地', value='紐約', start=6, end=8),
-                Entity(name='目的地', value='KIX', start=10, end=13, replacements=['斯堪地那維亞', '新加坡']),
-            ],
-            [
-                Entity(name='日期', value='下禮拜二', start=3, end=7, replacements=['明天']),
-                Entity(name='出發地', value='紐約', start=8, end=10),
-                Entity(name='目的地', value='KIX', start=12, end=15, replacements=['新加坡', '斯堪地那維亞']),
-            ],
-        ]
-        self.expected_entity_wo_replacements_lists = [
             [
                 Entity(name='日期', value='明天', start=3, end=5),
                 Entity(name='出發地', value='紐約', start=6, end=8),
@@ -113,10 +80,9 @@ class ExpandByEntitiesTestCase(TestCase):
         ]
         self.expected_data = []
         self.expected_data_without_replacement = []
-        for utt, ents, ents_wo_re, ints in zip(
+        for utt, ents, ints in zip(
                 self.expected_utterances,
                 self.expected_entity_lists,
-                self.expected_entity_wo_replacements_lists,
                 self.expected_intents,
             ):
             self.expected_data.append(Datum(
@@ -124,17 +90,11 @@ class ExpandByEntitiesTestCase(TestCase):
                 entities=ents,
                 intents=ints,
             ))
-            self.expected_data_without_replacement.append(Datum(
-                utterance=utt,
-                entities=ents_wo_re,
-                intents=ints,
-            ))
 
-    def test_correct_result_original_augmentation_method(self):
+    def test_expand_by_entiies(self):
         result = expand_by_entities(
             self.datum,
-            augment_method=augment_single_partition_utterances,
-            include_replacements=True,
+            include_orig=True,
         )
 
         # correct expand numbers
@@ -142,37 +102,51 @@ class ExpandByEntitiesTestCase(TestCase):
         for idx, datum in enumerate(result):
             with self.subTest(datum_idx=idx):
                 self.assertIn(datum, self.expected_data)
-
-    def test_correct_result_augmentation_with_upper_bound(self):
-        result = expand_by_entities(
-            self.datum,
-            augment_method=augment_single_partition_utterances_with_upper_bound,
-            augment_params={"upper_bound": 50},
-            include_replacements=True,
-        )
-
-        # correct expand numbers
-        self.assertEqual(6, len(result))
-        for idx, datum in enumerate(result):
-            with self.subTest(datum_idx=idx):
-                self.assertIn(datum, self.expected_data)
-
-    def test_correct_result_wo_replacement(self):
-        result = expand_by_entities(
-            self.datum,
-            augment_method=augment_single_partition_utterances,
-            include_replacements=False,
-        )
-
-        # correct expand numbers
-        self.assertEqual(6, len(result))
-        for idx, datum in enumerate(result):
-            with self.subTest(datum_idx=idx):
-                self.assertIn(datum, self.expected_data_without_replacement)
 
     def test_partition_by_entities(self):
-        entities, parts = partition_by_entities(self.datum)
+        actual_parts, entity_names = partition_by_entities(self.datum, False)
+        expected_parts = [
+            ['我想訂'],
+            ['下禮拜二'],
+            ['從'],
+            ['紐約'],
+            ['飛到'],
+            ['斯堪地那維亞', 'KIX'],
+            ['的機票'],
+        ]
+        for exp_part, act_part in zip(expected_parts, actual_parts):
+            self.assertEqual(set(exp_part), set(act_part))
         self.assertEqual(
-            parts,
-            ['我想訂', '明天', '從', '紐約', '飛到', '新加坡', '的機票'],
+            entity_names,
+            [None, '日期', None, '出發地', None, '目的地', None]
         )
+
+    def test_partition_by_entities_include_orig(self):
+        actual_parts, entity_names = partition_by_entities(self.datum, True)
+        expected_parts = [
+            ['我想訂'],
+            ['明天', '下禮拜二'],
+            ['從'],
+            ['紐約'],
+            ['飛到'],
+            ['新加坡', '斯堪地那維亞', 'KIX'],
+            ['的機票'],
+        ]
+        for exp_part, act_part in zip(expected_parts, actual_parts):
+            self.assertEqual(set(exp_part), set(act_part))
+        self.assertEqual(
+            entity_names,
+            [None, '日期', None, '出發地', None, '目的地', None]
+        )
+
+    def test__aggregate_entities(self):
+        segments = ['我想喝', '珍奶', '半糖']
+        entity_names = [None, 'DRINK', 'SUGAR']
+
+        expected_entities = [
+            Entity(name='DRINK', value='珍奶', start=3, end=5),
+            Entity(name='SUGAR', value='半糖', start=5, end=7),
+        ]
+
+        actual_entities = _aggregate_entities(segments, entity_names)
+        self.assertEqual(expected_entities, actual_entities)
