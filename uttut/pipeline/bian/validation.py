3ba2df7b57662db5b6d2c57a43208349612cf72a
Solumilken <s916526000@gmail.com>
2018-12-14 15:11:12 +0800

    validate edits and spans using validate_objs

--- uttut/pipeline/bian/validation.py
+++ uttut/pipeline/bian/validation.py
@@ -1,3 +1,6 @@
+from typing import Sequence
+
+
 def validate_start_end(start: int, end: int):
 
     if not isinstance(start, int) or not isinstance(end, int):
@@ -7,3 +10,36 @@ def validate_start_end(start: int, end: int):
     if start > end:
         raise ValueError("start cannot be greater than end")
     return start, end
+
+
+def validate_objs(objs: Sequence[object], type_):
+    '''
+    A object must have two integer properties `start` and `end`.
+    e.g.
+        1. validate a list of edits
+            validate_objs([Edit(), ...], Edit)
+        2. validate a list of spans
+            validate_objs([Span(), ...], Span)
+    '''
+    _validate_type_of_each_elements(objs, type_)
+    return _validate_disjoint(objs)
+
+
+def _validate_disjoint(objs: Sequence[object]):
+    '''
+    A object must have two integer properties `start` and `end`.
+    '''
+    objs = sorted(objs, key=lambda e: e.end)
+
+    current = objs[0].end
+    for obj in objs[1:]:
+        if obj.start < current:
+            raise ValueError(f"overlapped")
+        current = obj.end
+    return objs
+
+
+def _validate_type_of_each_elements(objs: Sequence[object], type_):
+    for i, element in enumerate(objs):
+        if not isinstance(element, type_):
+            raise TypeError(f"{i}-th element is not a(an) {type_.__name__}")
