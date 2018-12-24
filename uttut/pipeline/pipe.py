3907e5c93bea2e3d11b4ad4fed81d27396a5880c
Solumilken <s916526000@gmail.com>
2018-12-24 17:39:37 +0800

    added pipe

---
+++ uttut/pipeline/pipe.py
@@ -0,0 +1,97 @@
+from typing import List, Union
+
+import json
+
+from .step import Step
+from .factory import OperatorFactory
+
+
+class Pipe:
+
+    def __init__(self):
+        self._steps = []
+        self._step_info = []
+
+    def add(self, op_name: str, op_kwargs=None):
+        """Add steps based on the operation name.
+
+        This method create a steps, which has a op. The op
+        is created
+
+        Args:
+            op_name:
+            op_kwargs,
+
+        Raises:
+            KeyError
+
+        """
+        if op_kwargs is None:
+            op_kwargs = {}
+
+        op = OperatorFactory.from_name(op_name)(**op_kwargs)
+        step = Step(op)
+
+        self._validate_steps(step)
+
+        self.update_steps(step)
+        self.update_step_info(op_name, op_kwargs)
+
+    def _validate_steps(self, step):
+        if len(self._steps) > 0:
+            target_type = self._steps[-1].output_type
+            in_type = step.input_type
+            if in_type != target_type:
+                raise TypeError(
+                    "InputType of the step op is not valid."
+                    f"Got {in_type}, but requires {target_type}")
+
+    def update_steps(self, step: Step):
+        self._steps.append(step)
+
+    def update_step_info(self, name, kwargs):
+        self._step_info.append(
+            {  # for serialization
+                'op_name': name,
+                'op_kwargs': kwargs,
+            },
+        )
+
+    def transform(self, input_sequence: Union[str, List[str]], labels: List[int]):
+        """Process data based on Steps(Ops).
+
+        This method process input data according to the given
+        steps.
+
+        Args:
+            data:
+
+        Returns:
+            data: transfromed data
+        """
+        for step in self._steps:
+            input_sequence, labels = step.transform(input_sequence, labels)
+        return input_sequence, labels
+
+    def realign_labels(self, labels: List[int]) -> List[int]:
+        for step in self._steps[::-1]:
+            labels = step.realign_labels(labels)
+        return labels
+
+    def serialize(self, path):
+        with open(path, 'wb') as fw:
+            json.dump(self.step_info, fw)
+
+    @classmethod
+    def restore_from_json(cls, path: str):
+        pipe = cls()
+        with open(path, 'rb') as f:
+            step_infos = json.load(f)
+
+        # restore steps
+        for step_info in step_infos:
+            pipe.add(
+                op_name=step_info['op_name'],
+                op_kwargs=step_info['op_kwargs'],
+            )
+        return pipe
