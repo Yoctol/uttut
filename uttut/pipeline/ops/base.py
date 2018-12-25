164a86ef4eb0ddbe4740bd73545b0673dc7cf430
Solumilken <s916526000@gmail.com>
2018-12-25 18:18:46 +0800

    modified docstring

--- uttut/pipeline/ops/base.py
+++ uttut/pipeline/ops/base.py
@@ -6,8 +6,8 @@ class Operator(ABC):
     """Base class for Ops
     Sub-classes should implement `transform` and `relabel`.
     Attributes:
-        input_type: input type of datum to transform
-        output_type: output type of transformed datum
+        input_type: input type of sequence to transform
+        output_type: output type of transformed sequence
     """
 
     def __init__(self, input_type, output_type):
@@ -36,13 +36,16 @@ class Operator(ABC):
             labels: List[int],
             state: dict = None,
         ) -> Tuple[Union[str, List[str]], List[int]]:
-        """Transform input and label and Update self.edit
-        Transform input to certain form which meets the output_type
+        """Transform input_sequence and label and update self.edit
+
+        Transform input_sequence to certain form which meets the output_type
         and updates labels if necessary.
+
         Args:
             input_sequence (input_type): utterance or tokens
             labels (ints): has same length of input_sequence
             state (dict): data dependent information (output of fit)
+
         Returns:
             output (output_type): the transformed result
             labels (ints): has same length as output of transfrom
@@ -51,11 +54,14 @@ class Operator(ABC):
 
     @abstractmethod
     def realign_labels(self, labels: List[int], state: dict = None) -> List[int]:
-        """Propagate model labels back to original input
-        Note that self.edit should not be None.
-        Arg:
+        """Realign model labels to original input
+
+        Note that self.edit should not be None when calling this method.
+
+        Args:
             labels (ints): has same length as output of transfrom
             state (dict): data dependent information (output of fit)
+
         Return:
             labels (ints): has same length as input of transform
         """
