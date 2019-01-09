213a6f2c9f93243469b4dff3c48c833f9f5e7e3c
Solumilken <s916526000@gmail.com>
2019-01-09 17:45:55 +0800

    added step test cases

---
+++ uttut/pipeline/tests/test_step.py
@@ -0,0 +1,29 @@
+import pytest
+
+from ..ops.base import Operator
+from ..step import Step
+
+
+class MockedOperator(Operator):
+
+    def __init__(self):
+        super().__init__('in', 'out')
+
+    def transform(self, input_sequence, labels):
+        pass
+
+
+@pytest.fixture
+def step():
+    yield Step(MockedOperator())
+
+
+def test_transform(step, mocker):
+    mock_method = mocker.patch.object(step.op, 'transform')
+    step.transform('abc', [1, 2, 3])
+    mock_method.assert_called_once_with(input_sequence='abc', labels=[1, 2, 3])
+
+
+def test_attributes(step):
+    assert MockedOperator().input_type == step.input_type
+    assert MockedOperator().output_type == step.output_type
