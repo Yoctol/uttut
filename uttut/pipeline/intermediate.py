3eba76d239c3830d1a37a543bc369242abaa5ce1
Solumilken <s916526000@gmail.com>
2019-01-14 17:16:32 +0800

    added docstring

--- uttut/pipeline/intermediate.py
+++ uttut/pipeline/intermediate.py
@@ -3,19 +3,56 @@ from typing import List
 
 class Intermediate:
 
+    """Collect intermediate products when Pipe run transformation
+
+    attributes:
+        collection (list): store input intermediates
+        checkpoints (ints): indices of intermediates for query
+
+    """
+
     def __init__(self, checkpoints: List[int]):
         self.collection: list
         self.collection = []
         self.checkpoints = checkpoints
 
     def add(self, intermediate):
+        """Append intermediate into self.collection
+
+        Arg:
+            intermediate: object to be stored in self.collection
+
+        """
         self.collection.append(intermediate)
 
     def get(self, index: int = 0):
+        """Query intermediate in collection according to input index
+
+        Arg:
+            index (int)
+
+        Raise:
+            IndexError: list index out of range
+            if input index >= len(self.checkpoints) or
+            self.checkpoints(index) >= len(self.collection)
+
+        """
         if len(self.checkpoints) == 0:
             return []
         ck_index = self.checkpoints[index]
         return self.collection[ck_index]
 
-    def __getitem__(self, index: int):
-        return self.collection[index]
+    def __getitem__(self, key):
+        """Query intermediates by index or slice
+
+        Arg:
+            key (int or slice)
+
+        Return:
+            intermediates (list)
+
+        Raise:
+            IndexError: list index out of range if input index >= len(self.collection)
+
+        """
+        return self.collection[key]
