d75f1f1596c688a0f9ae9ed342c3791ad1a6b30c
Solumilken <s916526000@gmail.com>
2019-01-15 18:56:43 +0800

    added docstring

--- uttut/pipeline/ops/tests/common_tests.py
+++ uttut/pipeline/ops/tests/common_tests.py
@@ -4,11 +4,39 @@ pytest_parametrize = pytest.mark.parametrize
 
 
 def update_locals(input_locals, funcs):
+    """Modify input local symbol table"""
+
     input_locals.update({func.__name__: func for func in funcs})
 
 
 def common_test(test_cases):
+    """Common tests for Operators
 
+    Input pytest parametrization should have 4 input arguments, including
+    input_sequence, input_labels, output_sequence, output_labels.
+
+    Additional common tests can be added here as needed.
+
+    Arg:
+        test_cases (pytest.param): list of pytest parametrizations
+
+    Return
+        funcs : list of parametrized functions.
+
+    E.g.
+        common_test(
+            [
+                pytest.param(
+                    ['alvin', '喜歡', '吃', '榴槤'],
+                    [1, 2, 3, 4],
+                    ['<sos>', 'alvin', '喜歡', '吃', '榴槤', '<eos>'],
+                    [0, 1, 2, 3, 4, 0],
+                    id='zh',
+                ),
+            ]
+        )
+
+    """
     def test_data(input_sequence, input_labels, output_sequence, output_labels):
         assert len(input_sequence) == len(input_labels)
         assert len(output_sequence) == len(output_labels)
