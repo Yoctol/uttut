fa67faf061af6cb2d94092a8bcda3146b1a04674
Solumilken <s916526000@gmail.com>
2019-01-22 14:50:04 +0800

    added docstring

--- uttut/pipeline/ops/whitespace_tokenizer.py
+++ uttut/pipeline/ops/whitespace_tokenizer.py
@@ -6,10 +6,29 @@ from .label_transducer import get_most_common_except_not_entity
 
 class WhiteSpaceTokenizer(Tokenizer):
 
+    """
+    Spilt input string into pieces by whitespace characters
+
+    This is a modification of `whitespace_tokenize` in Bert `tokenization.py`.
+
+    E.g.
+    >>> from uttut.pipeline.ops.whitespace_tokenizer import WhiteSpaceTokenizer
+    >>> op = WhiteSpaceTokenizer()
+    >>> output_seq, output_labels, realigner = op.transform("a b", [1, 2, 3])
+    >>> output_seq
+    ["a", "b"]
+    >>> output_labels
+    [1, 3]
+    >>> realigner(output_labels)
+    [1, 0, 3]
+
+    """
+
     def __init__(self):
         super().__init__(WhiteSpaceTokenizerRealigner)
 
     def _tokenize(self, input_str: str) -> List[str]:
+
         """Split text into list by whitespace characters
 
         E.g.
