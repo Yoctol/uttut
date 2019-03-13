53e3cfc0d62352fd431bdc729be218a7db06b1f0
Solumilken <s916526000@gmail.com>
2019-03-13 12:04:45 +0800

    TrieNode has more methods

--- uttut/pipeline/ops/utils/trie.py
+++ uttut/pipeline/ops/utils/trie.py
@@ -1,8 +1,25 @@
 class TrieNode:
 
     def __init__(self):
-        self.children = {}
-        self.word = None
+        self._children = {}
+        self._word = None
+
+    def has_child(self, key):
+        if key in self._children:
+            return True
+        return False
+
+    def get_child(self, key):
+        return self._children[key]
+
+    def insert_child(self, key):
+        self._children[key] = TrieNode()
+
+    def set_word(self, word):
+        self._word = word
+
+    def get_word(self):
+        return self._word
 
 
 class Trie:
@@ -16,32 +33,32 @@ class Trie:
     def exactly_search(self, word: str):
         current = self.root
         for char in word:
-            if char not in current.children:
+            if not current.has_child(char):
                 return False
-            current = current.children[char]
-        if current.word:
+            current = current.get_child(char)
+        if current.get_word():
             return True
         return False
 
     def insert(self, word: str):
         current = self.root
         for char in word:
-            if char not in current.children:
-                current.children[char] = TrieNode()
-            current = current.children[char]
-        current.word = word
+            if not current.has_child(char):
+                current.insert_child(char)
+            current = current.get_child(char)
+        current.set_word(word)
 
     def match_prefix(self, word: str, shortest: bool = False, start_idx: int = 0):
         current = self.root
         longest_word = None
         for idx in range(start_idx, len(word)):
             char = word[idx]
-            if char not in current.children:
+            if not current.has_child(char):
                 return longest_word
-            current = current.children[char]
+            current = current.get_child(char)
 
-            if current.word:
-                longest_word = current.word
+            if current.get_word():
+                longest_word = current.get_word()
                 if shortest:
                     return longest_word
         return longest_word
