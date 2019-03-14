24a39143181550173f5fbc4a8b60c90701e7569d
noobOriented <jsaon92@gmail.com>
2019-03-14 15:08:03 +0800

    add OperatorTestTemplate.

--- uttut/pipeline/ops/tests/common_tests.py
+++ uttut/pipeline/ops/tests/common_tests.py
@@ -1,68 +1,55 @@
-import pytest
-
-pytest_parametrize = pytest.mark.parametrize
-
-
-def update_locals(input_locals, funcs):
-    """Modify input local symbol table"""
-
-    input_locals.update({func.__name__: func for func in funcs})
+import abc
+from collections import namedtuple
+from typing import List
 
 
-def common_test(test_cases):
+class OperatorTestTemplate(abc.ABC):
+
     """Common tests for Operators
 
     Input pytest parametrization should have 4 input arguments, including
     input_sequence, input_labels, output_sequence, output_labels.
-
-    Additional common tests can be added here as needed.
-
-    Arg:
-        test_cases (pytest.param): list of pytest parametrizations
-
-    Return
-        funcs : list of parametrized functions.
-
-    E.g.
-        common_test(
-            [
-                pytest.param(
-                    ['alvin', '喜歡', '吃', '榴槤'],
-                    [1, 2, 3, 4],
-                    ['<sos>', 'alvin', '喜歡', '吃', '榴槤', '<eos>'],
-                    [0, 1, 2, 3, 4, 0],
-                    id='zh',
-                ),
-            ]
-        )
-
     """
-    def test_data(input_sequence, input_labels, output_sequence, output_labels, op):
+    allowed_fixtures = ['input_sequence', 'input_labels', 'output_sequence', 'output_labels']
+    ParamTuple = namedtuple('ParamTuple', allowed_fixtures + ['id'])
+
+    @property
+    @abc.abstractmethod
+    def params(self) -> List[ParamTuple]:
+        pass
+
+    def pytest_generate_tests(self, metafunc):
+        # intersection
+        needed_args = list(set(self.allowed_fixtures) & set(metafunc.fixturenames))
+
+        def choose_needed_param(param):
+            return [getattr(param, key) for key in needed_args]
+
+        needed_params = [
+            choose_needed_param(param)
+            for param in self.params
+        ]
+        ids = [param.id for param in self.params]
+        metafunc.parametrize(needed_args, needed_params, ids=ids, scope="class")
+
+    @abc.abstractmethod  # fixture
+    def op(self):
+        pass
+
+    def test_data(self, input_sequence, input_labels, output_sequence, output_labels):
         assert len(input_sequence) == len(input_labels)
         assert len(output_sequence) == len(output_labels)
 
-    def test_transform(input_sequence, input_labels, output_sequence, output_labels, op):
+    def test_transform(self, input_sequence, output_sequence, op):
         output = op.transform(input_sequence)
         assert output_sequence == output[0]
 
-    def test_transform_labels(input_sequence, input_labels, output_sequence, output_labels, op):
+    def test_transform_labels(self, input_sequence, input_labels, output_labels, op):
         _, label_aligner = op.transform(input_sequence)
         output = label_aligner.transform(input_labels)
         assert output_labels == output
 
-    def test_inverse_transform_labels(
-            input_sequence,
-            input_labels,
-            output_sequence,
-            output_labels,
-            op,
-        ):
+    def test_inverse_transform_labels(self, input_sequence, input_labels, output_labels, op):
         _, label_aligner = op.transform(input_sequence)
         output = label_aligner.inverse_transform(output_labels)
         assert input_labels == output
-
-    argstr = "input_sequence,input_labels,output_sequence,output_labels"
-    local_objs = locals()
-    funcs = [local_objs[func]
-             for func in local_objs if callable(local_objs[func]) and func.startswith('test')]
-    return [pytest_parametrize(argstr, test_cases)(f) for f in funcs]
