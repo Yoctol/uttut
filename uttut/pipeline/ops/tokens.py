e0bcf2b7d8e77f93401a8344bb7c9236ca4cf19d
Solumilken <s916526000@gmail.com>
2019-06-13 15:06:45 +0800

    num token with space

--- uttut/pipeline/ops/tokens.py
+++ uttut/pipeline/ops/tokens.py
@@ -1,9 +1,10 @@
 # numerical tokens
 INT_TOKEN = "_int_"
 FLOAT_TOKEN = "_float_"
+NUM_TOKEN = "_num_"
 INT_TOKEN_WITH_SPACE = f" {INT_TOKEN} "
 FLOAT_TOKEN_WITH_SPACE = f" {FLOAT_TOKEN} "
-
+NUM_TOKEN_WITH_SPACE = f" {NUM_TOKEN} "
 
 #
 START_TOKEN = "<sos>"
